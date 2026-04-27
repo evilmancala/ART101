16376d07f95c834865db0c91c4eeb616a50bed4b
evilmancala <ner0tic77@gmail.com>
2026-04-27 16:23:01 -0700

    add

---
+++ exercises/en02.js
@@ -0,0 +1,8 @@
+function press() {
+  var x = document.getElementById("hide");
+  if (x.style.display === "hidden") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "hidden";
+  }
+}
