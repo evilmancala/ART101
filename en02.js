b6a614b40058e83b636d6dc98458b5d0613033d5
evilmancala <ner0tic77@gmail.com>
2026-05-18 02:31:29 -0700

    death

--- en02.js
+++
@@ -1,8 +0,0 @@
-function press() {
-  var x = document.getElementById("hide");
-  if (x.style.display === "hidden") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "hidden";
-  }
-}
